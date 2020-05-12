4578218eb96f377bb3e316783e10ceff46d28c37
mateusz <dalvan234@gmail.com>
2020-05-12 22:33:39 +0200

    calendar arrows size fixed, changing goals component total macros row added, changing goals page appearance changed

--- src/nutrition/nutrition.jsx
+++ src/nutrition/nutrition.jsx
@@ -9,11 +9,13 @@ class nutrition extends Component {
             calories: 0,
             carbs: 0,
             fats: 0,
-            protein: 0
+            protein: 0,
+            total: 0,
+            id: null
         }
     }
     componentDidMount() {
-        if(this.props.users[this.props.id]){
+        if (this.props.users[this.props.id]) {
             this.setState({
                 calories: this.props.users[this.props.id].calories,
                 carbs: this.props.users[this.props.id].carbs,
@@ -21,6 +23,7 @@ class nutrition extends Component {
                 protein: this.props.users[this.props.id].protein,
             })
         }
+
     }
     inputChange(e, array) {
         this.setState({
@@ -32,7 +35,7 @@ class nutrition extends Component {
             fetch('http://localhost:3000/users/' + this.props.id, {
                 method: 'PUT',
                 body: JSON.stringify({
-                    
+
                     name: this.props.users[this.props.id].name,
                     mail: this.props.users[this.props.id].mail,
                     password: this.props.users[this.props.id].password,
@@ -45,54 +48,77 @@ class nutrition extends Component {
                     "Content-type": "application/json; charset=UTF-8"
                 }
             })
-            //window.location.reload();
         } else {
             alert('macronutrients must sum up to 100%')
         }
 
     }
     render() {
-        if(this.props.users[this.props.id]){
+        let id;
+        if ((parseInt(this.state.carbs) + parseInt(this.state.fats) + parseInt(this.state.protein) === 100)) {
+            id = "green-id";
+        } else {
+            id = "red-id";
+        }
+        if (this.props.users[this.props.id]) {
             let optionsArray = [];
-        for (let i = 0; i < 21; i++) {
-            optionsArray.push(<option value={i * 5}>{i * 5}%</option>);
-        }
-        const line = (type, value, selectValue, stateValue) => {
-            return (<div className="settings-line">
-                <div className="settings-left">
-                    <h1>{type} {value}</h1>
-                </div>
-                <div className="settings-right">
-                    <select value={selectValue} onChange={(e) => this.inputChange(e, stateValue)}>
-                        {optionsArray}
-                    </select>
-                </div>
-            </div>);
-        }
-        return (
-            <div className="goals-content">
-                <div className="nutrition-settings">
-                    <div className="settings-line">
-                        <div className="settings-left">
-                            <h1>Daily Nutrition Goals</h1>
+            for (let i = 0; i < 21; i++) {
+                optionsArray.push(<option value={i * 5}>{i * 5}%</option>);
+            }
+            const line = (type, value, selectValue, stateValue) => {
+                return (<div className="settings-line">
+                    <div className="settings-left">
+                        <h1>{type} {value}</h1>
+                    </div>
+                    <div className="settings-right">
+                        <select value={selectValue} onChange={(e) => this.inputChange(e, stateValue)}>
+                            {optionsArray}
+                        </select>
+                    </div>
+                </div>);
+            }
+            return (
+                <div className="goals-content">
+                    <div className="nutrition-settings">
+                        <div className="topic-line">
+                            <div className="settings-left">
+                                <h1>Editing Goals</h1>
+                            </div>
+                            <div className="settings-right">
+                                <Link to="/goals" style={{ textDecoration: 'none', color: "rgb(76, 145, 235)" }}><button onClick={() => this.changeCalories()}>Save Changes</button></Link>
+                            </div>
                         </div>
-                        <div className="settings-right">
-                            <input value={this.state.calories} type="text" onChange={(e) => this.inputChange(e, "calories")}></input>
+                        <div className="settings-line">
+                            <div className="settings-left">
+                                <h1>Daily Nutrition Goals</h1>
+                            </div>
+                            <div className="settings-right">
+                                <input value={this.state.calories} type="text" onChange={(e) => this.inputChange(e, "calories")}></input>
+                            </div>
+                        </div>
+                        {line("Carbohydrates", Math.ceil(this.props.users[this.props.id].calories * this.props.users[this.props.id].carbs / 100 / 4) + " g", this.state.carbs, "carbs")}
+                        {line("Fats", Math.ceil(this.props.users[this.props.id].calories * this.props.users[this.props.id].fats / 100 / 9) + " g", this.state.fats, "fats")}
+                        {line("Protein", Math.ceil(this.props.users[this.props.id].calories * this.props.users[this.props.id].protein / 100 / 4) + " g", this.state.protein, "protein")}
+                        <div className="settings-line">
+                            <div className="settings-left">
+                                <div className="line-column">
+                                <h1 >Total</h1>
+                                <h1>Macronutrients must equal 100%</h1>
+                                </div>
+                                
+                            </div>
+                            <div className="settings-right">
+                                <h1 id={id}>{(parseInt(this.state.carbs) + parseInt(this.state.fats) + parseInt(this.state.protein)) + "%"}</h1>
+                            </div>
                         </div>
                     </div>
-                    {line("Carbohydrates", Math.ceil(this.props.users[this.props.id].calories * this.props.users[this.props.id].carbs / 100 / 4) + " g", this.state.carbs, "carbs")}
-                    {line("Fats", Math.ceil(this.props.users[this.props.id].calories * this.props.users[this.props.id].fats / 100 / 9) + " g", this.state.fats, "fats")}
-                    {line("Protein", Math.ceil(this.props.users[this.props.id].calories * this.props.users[this.props.id].protein / 100 / 4) + " g", this.state.protein, "protein")}
-
                 </div>
-                <Link to="/goals" style={{ textDecoration: 'none', color: "rgb(76, 145, 235)" }}><button onClick={() => this.changeCalories()}>Save Changes</button></Link>
-            </div>
 
-        )
-        }else{
-            return(null);
+            )
+        } else {
+            return (null);
         }
-        
+
     }
 }
 
